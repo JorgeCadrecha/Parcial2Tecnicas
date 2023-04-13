c5ad94c3255fec777bec6a19aae6fad6342ff544
Jorge <jcadrdel@myuax.es>
2023-04-13 12:44:08 +0200

    Test de las clases Ship y User

---
+++ src/test/java/Battleship/ShipTest.java
@@ -0,0 +1,46 @@
+package Battleship;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShipTest {
+
+    @org.junit.jupiter.api.Test
+    void getSize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getStart() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEnd() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDirection() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getHits() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isSunk() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setHits() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSunk() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isAlreadyHit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getShot() {
+    }
+}
---
+++ src/test/java/Battleship/UserTest.java
@@ -0,0 +1,36 @@
+package Battleship;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void getShips() {
+    }
+
+    @Test
+    void setShips() {
+    }
+
+    @Test
+    void setAlive() {
+    }
+
+    @Test
+    void isAlive() {
+    }
+
+    @Test
+    void die() {
+    }
+
+    @Test
+    void attack() {
+    }
+
+    @Test
+    void getShot() {
+    }
+}

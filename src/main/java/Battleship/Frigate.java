5ff7791e917a42c10512bde4874e64c796b632fa
Jorge <jcadrdel@myuax.es>
2023-04-13 12:32:21 +0200

    Clases Battleship, Canoe y Frigate

---
+++ src/main/java/Battleship/Frigate.java
@@ -0,0 +1,28 @@
+package Battleship;
+
+public class Frigate extends Ship{
+    private int size;
+    private int hits;
+
+    public Frigate(Point start, Point end) throws Exception {
+        super(start, end);
+        this.size = 2;
+        this.hits = 0;
+    }
+
+    public int get_size() {
+        return this.size;
+    }
+
+    public int get_hits() {
+        return this.hits;
+    }
+
+    public void hit() {
+        this.hits++;
+    }
+
+    public boolean is_sunk() {
+        return this.hits == this.size;
+    }
+}
